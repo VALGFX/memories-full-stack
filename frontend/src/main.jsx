5e681d928d247ddb81cdabe5905440d61b7f58dc
Valeriu Mazur <160285130+VALGFX@users.noreply.github.com>
2025-01-31 12:06:30 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ShopContextProvider>
+      <Analytics/>
       <App />
     </ShopContextProvider>
   </BrowserRouter>,
