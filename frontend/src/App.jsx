dc33f563e7df3e98c82be085b7daeb0a00e48f28
Valeriu Mazur <160285130+VALGFX@users.noreply.github.com>
2025-02-06 11:39:53 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,54 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import Header from './components/Header'
-import SearchBar from './components/SearchBar'
-import About from './pages/About'
-import Cart from './pages/Cart'
-import Collection from './pages/Collection'
-import Contact from './pages/Contact'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Orders from './pages/Orders'
-import PlaceOrder from './pages/PlaceOrder'
-import Product from './pages/Product'
-import Promotions from './pages/Promotions'
-import Verify from './pages/Verify'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import SearchBar from './components/SearchBar';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import PlaceOrder from './pages/PlaceOrder';
+import Orders from './pages/Orders';
+import Verify from './pages/Verify';
+import Promotions from './pages/Promotions';
 
 const App = () => {
-	return (
-		<div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-			<ToastContainer />
-			<Header />
-			<Navbar />
-			<SearchBar />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/collection' element={<Collection />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/contact' element={<Contact />} />
-				<Route path='/product/:productId' element={<Product />} />
-				<Route path='/cart' element={<Cart />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/place-order' element={<PlaceOrder />} />
-				<Route path='/orders' element={<Orders />} />
-				<Route path='/verify' element={<Verify />} />
-				<Route path='/promotions' element={<Promotions />} />
-			</Routes>
-			<Footer />
-		</div>
-	)
-}
+  return (
+    <Router>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <ToastContainer position="top-right" autoClose={3000} />
+        <Header />
+        <Navbar />
+        <SearchBar />
+        
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/collection" element={<Collection />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/place-order" element={<PlaceOrder />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/verify" element={<Verify />} />
+            <Route path="/promotions" element={<Promotions />} />
+          </Routes>
+        </main>
 
-export default App
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
