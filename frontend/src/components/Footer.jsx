f7d04e2414a35ffe8b05015e2ed94ea1ccedb2ef
Valeriu Mazur <160285130+VALGFX@users.noreply.github.com>
2025-02-12 21:10:57 +0200

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 		<div>
 			<div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 				<div>
-					<img src={assets.logo} className='mb-5 w-32' alt='' />
+					<img src={assets.logo_img} className='mb-5 w-32' alt='' />
 					<p className='w-full md:w-2/3 text-gray-600'>
 						La Memories, ne dedicăm furnizării celor mai bune produse și
 						servicii pentru clienții noștri. Misiunea noastră este de a aduce
