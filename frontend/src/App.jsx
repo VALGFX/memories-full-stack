b61e46e48ac4b2ade53a582bf6368483d06cfdbb
Valeriu Mazur <160285130+VALGFX@users.noreply.github.com>
2025-02-12 21:11:20 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Footer from './components/Footer'
+import Header from './components/Header'
 import Navbar from './components/Navbar'
 import SearchBar from './components/SearchBar'
 import About from './pages/About'
@@ -19,24 +20,27 @@ import Verify from './pages/Verify'
 
 const App = () => {
 	return (
-		<div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+		<div>
 			<ToastContainer />
+			<Header />
 			<Navbar />
-			<SearchBar />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/collection' element={<Collection />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/contact' element={<Contact />} />
-				<Route path='/product/:productId' element={<Product />} />
-				<Route path='/cart' element={<Cart />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/place-order' element={<PlaceOrder />} />
-				<Route path='/orders' element={<Orders />} />
-				<Route path='/verify' element={<Verify />} />
-				<Route path='/promotions' element={<Promotions />} />
-			</Routes>
-			<Footer />
+			<div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+				<SearchBar />
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/collection' element={<Collection />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='/product/:productId' element={<Product />} />
+					<Route path='/cart' element={<Cart />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/place-order' element={<PlaceOrder />} />
+					<Route path='/orders' element={<Orders />} />
+					<Route path='/verify' element={<Verify />} />
+					<Route path='/promotions' element={<Promotions />} />
+				</Routes>
+				<Footer />
+			</div>
 		</div>
 	)
 }
