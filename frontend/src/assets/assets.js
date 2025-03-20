123e76b92482859e2806eea8b53b822b3d1c3d57
Valeriu Mazur <160285130+VALGFX@users.noreply.github.com>
2025-03-20 11:57:29 +0200

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -24,6 +24,7 @@ import stripe_logo from './stripe_logo.png'
 import student_img from './student_img.jpg'
 import support_img from './support_img.png'
 import phone_icon from './Vector.png'
+import spring_imgs from './IMG_9543.png'
 
 // Import product images
 import p_img1 from './p_img1.png'
@@ -71,6 +72,7 @@ export const assets = {
 	student_img,
 	spring_img,
 	christmas_img,
+	spring_imgs,
 }
 
 export const products = [
